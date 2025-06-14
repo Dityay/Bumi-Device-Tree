a52ffbec01b8ce046c1b8cba81bd4aab8c2bd70c
AbuRider <dinarysf276@gmail.com>
2025-06-14 18:39:03 +0700

    Update derp_earth.mk

--- derp_earth.mk
+++ derp_earth.mk
@@ -16,6 +16,7 @@ $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
 DERP_BUILDTYPE := Community
+TARGET_NOT_USES_BLUR := true
 
 PRODUCT_NAME := derp_earth
 PRODUCT_DEVICE := earth
