d8ac15226b141608c8f684890870adfa1e9db96c
AbuRider <dinarysf276@gmail.com>
2025-06-15 16:30:22 +0700

    Create Android.mk

---
+++ nuke-apps/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := nuke-apps
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Accord \
+    AndroidAutoStubPrebuilt \
+    Chrome64 \
+    Chrome \
+    Chrome-Stub \
+    Drive \
+    GoogleTTS \
+    Maps \
+    Photo \
+    PersonalSafety \
+    SafetyHubPrebuilt \
+    SetupWizardPrebuilt \
+    Velvet \
+    WellbeingPrebuilt \
+    YouTube
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
