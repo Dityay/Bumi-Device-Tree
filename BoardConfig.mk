fe4b25a322eadd2794e692d2bc8ed2bae27e9910
Dtyaa <100247109+Dityay@users.noreply.github.com>
2025-06-15 17:52:46 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,3 +189,5 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # Inherit the proprietary files
 include vendor/xiaomi/earth/BoardConfigVendor.mk
+
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
