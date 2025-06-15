038d3f27da0825c72aa38095fa65fc3a20028119
AbuRider <dinarysf276@gmail.com>
2025-06-15 16:22:29 +0700

    Update Android.mk

--- nuke-apps/Android.mk
+++ nuke-apps/Android.mk
@@ -1,1 +1,24 @@
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := nuke-apps
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Accord \
+    AndroidAutoStubPrebuilt \
+    Chrome64 \
+    Chrome \
+    Chrome-Stub \
+    Drive \
+    GoogleTTS \
+    Maps \
+    Photo \
+    PersonalSafety \
+    SafetyHubPrebuilt \
+    Velvet \
+    YouTube
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
